1e99a04aabd1d8ed7227d3b51a1fcb2c8b2d35c0
yejj002 <yejj002@lianlianpay.com>
2020-05-08 19:21:18 +0800

    first commit

---
+++ src/test/java/com/yejj/finance/funds/FinanceFundsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yejj.finance.funds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceFundsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
